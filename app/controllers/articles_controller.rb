528b379bbbea4cde8f1b74c371e022da1518e430
dmuktesh <muktesh@idyllic-software.com>
2016-07-13 12:22:19 +0530

    update the record commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,13 @@ class ArticlesController < ApplicationController
 	def new
 		@article=Article.new
 	end
+	def edit
+		@article = Article.find(params[:id])
+	end
+	def show
+		@article = Article.find(params[:id])
+	end
+
 	def create
 		#render plain: params[:article].inspect
 		#@article=Article.new(params[:article])
@@ -16,8 +23,14 @@ class ArticlesController < ApplicationController
 			render 'new'
 		end
 	end
-	def show
+	
+	def update
 		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'edit'
+		end
 	end
 	private
 		def article_params
