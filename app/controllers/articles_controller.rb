bcd027e8bc55c60d20e77995370c7351732bd897
dmuktesh <muktesh@idyllic-software.com>
2016-07-13 11:55:17 +0530

    error msg display

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,14 +3,18 @@ class ArticlesController < ApplicationController
 		@articles= Article.all
 	end
 	def new
+		@article=Article.new
 	end
 	def create
 		#render plain: params[:article].inspect
 		#@article=Article.new(params[:article])
 		#@article = Article.new(params.require(:article).permit(:title,:text))
 		@article = Article.new(article_params)
-		@article.save
-		redirect_to @article
+		if @article.save
+			redirect_to @article
+		else
+			render 'new'
+		end
 	end
 	def show
 		@article = Article.find(params[:id])
