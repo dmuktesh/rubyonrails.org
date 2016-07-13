90f74fa08df81c4a171ad0ef58d3b90f6f104634
dmuktesh <muktesh@idyllic-software.com>
2016-07-13 11:13:10 +0530

    save and show articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,22 @@
 class ArticlesController < ApplicationController
+	def index
+		@articles= Article.all
+	end
 	def new
 	end
 	def create
-		render plain: params[:article].inspect
+		#render plain: params[:article].inspect
+		#@article=Article.new(params[:article])
+		#@article = Article.new(params.require(:article).permit(:title,:text))
+		@article = Article.new(article_params)
+		@article.save
+		redirect_to @article
 	end
+	def show
+		@article = Article.find(params[:id])
+	end
+	private
+		def article_params
+			params.require(:article).permit(:title,:text)
+		end
 end
